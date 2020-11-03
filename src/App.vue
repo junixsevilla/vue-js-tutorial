c679e68e661914bdc1cd2a9df6d0d1b440767c68
Junix <jaranco@gmail.com>
2020-11-04 02:31:00 +1300

    deploy

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Command Center." />
+    <HelloWorld msg="Mission 3" />
   </div>
 </template>
 
